ece5d07630df22ab6b36019e548954e0159d6ab1
nv6281-cmd <nv6281@srmist.edu.in>
2026-03-25 13:35:04 +0530

    UC6 added

---
+++ HelloAppUC6.java
@@ -0,0 +1,25 @@
+public class HelloAppUC6 {
+
+    public static void main(String[] args) {
+
+        // Case 1: No arguments
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        // Case 2: Arguments provided
+        StringBuilder nameBuilder = new StringBuilder();
+
+        // Enhanced for loop to append names
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        // Remove trailing ", "
+        if (nameBuilder.length() > 0) {
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
